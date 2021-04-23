d1e8acf41e80e9220b583e77f9c47d383019aa9f
Dhrumil Mistry <dhrumilmistry2015@gmail.com>
2021-04-23 18:33:14 +0530

    add search entry, warning

--- GUI.py
+++ GUI.py
@@ -18,24 +18,35 @@ def get_country_number(country_name, received_data):
             # print(data)
             return number
         number += 1
+    return -1
 
 
 def get_data():
     print('Fetching Data..... Please be patient...')
+    
     covid_data = requests.get(API).json()
-    country_name = 'India'
+    country_name = data['country'].get()
+    country_name = country_name.lower()
+    country_name = country_name.title()
+    print(country_name)
+
     country_num = get_country_number(country_name, covid_data)
-    data['confirmed'].set(covid_data['data'][country_num]['confirmed'])
-    data['dead'].set(covid_data['data'][country_num]['dead'])
-    data['recovered'].set(covid_data['data'][country_num]['recovered'])
+    if country_num == -1:
+        init_values()
+        warning.grid(row=7, column=1)
+    else:
+        data['confirmed'].set(covid_data['data'][country_num]['confirmed'])
+        data['dead'].set(covid_data['data'][country_num]['dead'])
+        data['recovered'].set(covid_data['data'][country_num]['recovered'])
+        warning.grid_remove()
 
 
 # -- GUI ---
 root = Tk()
 root.title("World Covid Cases")
-root.geometry("480x234")
-root.maxsize(480,234)
-root.minsize(480,234)
+root.geometry("720x480")
+# root.maxsize(480,234)
+# root.minsize(480,234)
 
 root.iconbitmap('virus.ico')
 
@@ -45,8 +56,10 @@ Label(root, text='Country :', font='Comicsansms 13').grid(row=1, column=0)
 Label(root, text='Confirmed : ', font='Comicsansms 13').grid(row=2, column=0)
 Label(root, text='Dead : ', font='Comicsansms 13').grid(row=3, column=0)
 Label(root, text='Recovered : ', font='Comicsansms 13').grid(row=4, column=0)
+warning = Label(root, text='Please Enter Valid \nCountry and \ndo not use short names', fg='red',font='Comicsansms 13 bold', padx=5, pady=5)
 
-# tkinter variables
+
+# creating and initializing tkinter variables
 data = {
     'country': StringVar(),
     'confirmed': StringVar(),
@@ -55,6 +68,7 @@ data = {
 }
 
 init_values()
+
 
 # Entry for Country Name
 Entry(root, textvariable=data['country'], font='Comicsansms 13').grid(row=1, column=1)
@@ -67,7 +81,6 @@ Label(root, textvariable=data['recovered'], font='Comicsansms 13').grid(row=4, c
 
 
 # Button
-Button(root, text="Fetch Data", command=get_data,
-       padx=5, pady=5).grid(row=5, column=3)
+Button(root, text="Fetch Data", command=get_data, padx=5, pady=5).grid(row=5, column=3)
 
 root.mainloop()
